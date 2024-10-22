700721224ff44d00e913ee44c6a4ed121e07ee93
justjack1521 <henryt1521@hotmail.com>
2024-10-22 23:34:39 +1100

    update max message size?

--- internal/adapter/handler/web/client.go
+++ internal/adapter/handler/web/client.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	maxMessageSize  int64 = 512
+	maxMessageSize  int64 = 1024
 	writeWait             = 10 * time.Second
 	pongWait              = 60 * time.Second
 	pingPeriod            = (pongWait * 9) / 10
