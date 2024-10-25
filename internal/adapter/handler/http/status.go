95cf94ab9210b4d4fa53f64037603607d5072002
justjack1521 <henryt1521@icloud.com>
2024-10-25 15:42:24 +1100

    changed status codes

--- internal/adapter/handler/http/status.go
+++ internal/adapter/handler/http/status.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"mevway/internal/core/port"
 	"net"
+	"net/http"
 )
 
 type StatusHandler struct {
@@ -18,18 +19,18 @@ func (h *StatusHandler) Get(ctx *gin.Context) {
 
 	host, _, err := net.SplitHostPort(ctx.Request.RemoteAddr)
 	if err != nil {
-		ctx.AbortWithError(503, err)
+		ctx.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
 
 	var addr = net.ParseIP(host)
 	if addr == nil {
-		ctx.AbortWithStatus(503)
+		ctx.AbortWithStatus(http.StatusInternalServerError)
 		return
 	}
 
 	if err := h.svc.Status(addr); err != nil {
-		ctx.AbortWithError(503, err)
+		ctx.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
 
